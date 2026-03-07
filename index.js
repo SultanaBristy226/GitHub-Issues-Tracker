320c8053a9df0b203b7bcb399238310973260aa8
Habiba Sultana Bristy <bristysultana4@gmail.com>
2026-03-07 14:46:55 +0600

    third commit

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("login-button").addEventListener('click', function() {
+    const userInput = document.getElementById('username-input');
+    const userValue = userInput.value;
+    const passwordInput = document.getElementById("password-input");
+    const passwordValue = passwordInput.value;
+    
+    if (userValue === "admin" && passwordValue === "admin123") {
+        window.location.assign("./home.html");
+    } else {
+        alert("Enter valid username and password");
+        userInput.value = "";
+        passwordInput.value = "";
+    }
+});
